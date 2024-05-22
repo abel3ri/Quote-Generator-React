79eb0a717e434ac5cb38bb51a9954152e5503f1c
abel3ri <abelmerete@yahoo.com>
2024-05-22 22:11:46 +0300

    finish quote genrator content with styling

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/Quote.css";
+
 export default function Quote() {
+  const [quote, setQuote] = useState({ author: "", text: "" });
+
+  useEffect(() => {
+    async function fetchInitialQuote() {
+      const quote = (
+        await axios.get("http://inspo-quotes-api.herokuapp.com/quotes/random")
+      ).data.quote;
+      setQuote(quote);
+    }
+    fetchInitialQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    const quote = (
+      await axios.get("http://inspo-quotes-api.herokuapp.com/quotes/random")
+    ).data.quote;
+    setQuote(quote);
+  };
+  if (quote.author == "") {
+    return (
+      <>
+        <div className="loader"></div>
+      </>
+    );
+  }
   return (
-    <>
-      <h1>Quote Generator</h1>
-    </>
+    <div className="Quote">
+      <div>
+        <h3>"{quote.text}</h3>
+        <h5> - {quote.author}</h5>
+      </div>
+      <button onClick={fetchQuote} className="btn btn-random">
+        Generate
+      </button>
+    </div>
   );
 }
